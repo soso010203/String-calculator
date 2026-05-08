4f9b26f7ef4b7cf356a7eec04ac9329379a27f3a
Sophia Pluy <if25b259@technikum-wien.at>
2026-05-08 10:45:18 +0200

    Failing Test for Testcase 2

--- spec/string_calculator_spec.js
+++ spec/string_calculator_spec.js
@@ -6,4 +6,7 @@ describe('String Calculator', function() {
 	it('should return 0 for an empty string', function() {
 		expect(calculator.add('')).toEqual(0);
 	});
+	it('should return the number for a single number string', function() {
+		expect(calculator.add('1')).toEqual(1);
+	});
 });
