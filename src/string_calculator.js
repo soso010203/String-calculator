b8ad21198ee8e5ced18c07fe7be14b0399784f5f
Sophia Pluy <if25b259@technikum-wien.at>
2026-05-08 10:50:44 +0200

    Implemented single number string (Testcase 2)

--- src/string_calculator.js
+++ src/string_calculator.js
@@ -1,5 +1,13 @@
 // StringCalculator.js
 function StringCalculator() {}
-StringCalculator.prototype.add = function(string_numbers) {
-	return 0;
+StringCalculator.prototype.add = function(string_numbers) 
+{
+	if(string_numbers === '')
+	{
+		return 0;
+	}
+	else
+	{
+		return parseInt(string_numbers); //wandelt string in int um
+	}
 };
