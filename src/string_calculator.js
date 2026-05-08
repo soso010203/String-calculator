418157b5f2e526f3979e17a5403d22f7bcfe4c69
Sophia Pluy <if25b259@technikum-wien.at>
2026-05-08 11:18:00 +0200

    Implemented sum of three numbers (Testcase 5)

--- src/string_calculator.js
+++ src/string_calculator.js
@@ -8,9 +8,15 @@ StringCalculator.prototype.add = function(string_numbers)
 	}
 	else if(string_numbers.includes(','))
 	{
-		let numbers = string_numbers.split(','); //zerteilt die 2 Zahlen
+		let numbers = string_numbers.split(',');
+		let sum = 0;
 
-		return parseInt(numbers[0]) + parseInt(numbers[1]);
+		for (let i = 0; i < numbers.length; i++)
+		{
+			sum += parseInt(numbers[i], 10);
+		}
+
+		return sum;
 	}
 	else
 	{
