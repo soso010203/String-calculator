1f7368911a48d99742b5a56dc772673b8646fc90
Sophia Pluy <if25b259@technikum-wien.at>
2026-05-08 11:04:33 +0200

    Implemented sum of two numbers (Testcase 3)

--- src/string_calculator.js
+++ src/string_calculator.js
@@ -6,6 +6,12 @@ StringCalculator.prototype.add = function(string_numbers)
 	{
 		return 0;
 	}
+	else if(string_numbers.includes(','))
+	{
+		let numbers = string_numbers.split(','); //zerteilt die 2 Zahlen
+
+		return parseInt(numbers[0]) + parseInt(numbers[1]);
+	}
 	else
 	{
 		return parseInt(string_numbers); //wandelt string in int um
